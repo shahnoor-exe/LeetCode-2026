beb8cd46ef4aeeda84af9c329c0776a2e0af7d02
shahnoor-exe <shahnoor-exe@users.noreply.github.com>
2026-02-27 05:28:45 +0000

    Code challenge submission for question: 110 Balanced Binary Tree

---
+++ 110 Balanced Binary Tree/110balanced-binary-tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return check(root) != -1;
+    }
+
+    private int check(TreeNode node) {
+        if (node == null) return 0;
+        int left  = check(node.left);
+        int right = check(node.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) return -1;
+        return 1 + Math.max(left, right);
+    }
+}
