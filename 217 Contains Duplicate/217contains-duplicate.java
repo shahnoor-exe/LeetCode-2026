88b49648b84ca326c2d81c24f7620e5fb515d48b
shahnoor-exe <shahnoor-exe@users.noreply.github.com>
2026-02-27 05:12:37 +0000

    Code challenge submission for question: 217 Contains Duplicate

---
+++ 217 Contains Duplicate/217contains-duplicate.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int n : nums)
+            if (!seen.add(n)) return true;
+        return false;
+    }
+}
