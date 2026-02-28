1acea33c40ad46061ee1b89b55509c7e1ffce851
shahnoor-exe <shahnoor-exe@users.noreply.github.com>
2026-02-28 16:25:19 +0000

    Code challenge submission for question: 8 String to Integer (atoi)

---
+++ 8 String to Integer (atoi)/8string-to-integer-atoi.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0, n = s.length();
+        long result = 0;
+        int sign = 1;
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i < n && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return (int) (sign * result);
+    }
+}
