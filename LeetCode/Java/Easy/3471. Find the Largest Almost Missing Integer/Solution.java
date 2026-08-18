bd7ce24c7a26a89a388295fba79a6289754dc86b
Shahnoor Ahmed Laskar <182213730+shahnoor-exe@users.noreply.github.com>
2026-08-18 21:55:33 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/3471. Find the Largest Almost Missing Integer/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        int n=nums.length;
+        int[] c=new int[51];
+        for(int x:nums) c[x]++;
+        if(k==1){
+            for(int i=50;i>=0;i--){
+                if(c[i]==1) return i;
+            }return -1;
+        }
+        else if(k==n){
+            int y=-1;
+            for(int x:nums) return Math.max(x,y);
+        }
+        int y=-1;
+        if(c[nums[0]]==1) y=Math.max(y,nums[0]);
+        if(c[nums[n-1]]==1) y=Math.max(y,nums[n-1]);
+        return y;
+    }
+}
