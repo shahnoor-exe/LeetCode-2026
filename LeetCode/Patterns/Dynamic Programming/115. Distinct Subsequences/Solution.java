d1bb4a819a72ce4cd650f12aa048cef3da2905df
Shahnoor Ahmed Laskar <182213730+shahnoor-exe@users.noreply.github.com>
2026-09-06 23:38:15 +0530

    Add Hard Java Solution for 115. Distinct Subsequences from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/115. Distinct Subsequences/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m=s.length();
+        int n=t.length();
+        if(n>m) return 0;
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        for(int i=1;i<=m;i++){
+            for(int j=n;j>=1;j--){
+                if(s.charAt(i-1)==t.charAt(j-1)) dp[j]=dp[j]+dp[j-1];
+            }
+        }return dp[n];
+    }
+}
