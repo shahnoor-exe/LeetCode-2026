a94df732ff94515ce22d7caa358f5138bf8ee37a
Shahnoor Ahmed Laskar <182213730+shahnoor-exe@users.noreply.github.com>
2026-09-09 00:33:17 +0530

    Add Easy Java Solution for 4245. Count Commas in Range from LeetCode

---
+++ LeetCode/Java/Easy/4245. Count Commas in Range/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countCommas(int n) {
+        int c=0;
+        long x=1000;
+        while(n>=x){
+            c+=(n-x+1);
+            x+=1000;
+        }return c;
+    }
+}
