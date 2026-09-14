04c6c3300aab41cf3535eb374010a857d202aa3f
Shahnoor Ahmed Laskar <182213730+shahnoor-exe@users.noreply.github.com>
2026-09-15 01:49:20 +0530

    Add Easy Java Solution for 866. Rectangle Overlap from LeetCode

--- LeetCode/Patterns/Math/866. Rectangle Overlap/Solution.java
+++ LeetCode/Patterns/Math/866. Rectangle Overlap/Solution.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        return rec1[0]<rec2[1] && rec2[0]<rec1[2] && rec1[1]<rec2[3] && rec2[1]<rec1[3];
+        int l=Math.max(rec1[0],rec2[0]);
+        int r=Math.min(rec1[2],rec2[2]);
+        int b=Math.max(rec1[1],rec2[1]);
+        int t=Math.min(rec1[3],rec2[3]);
+        return l<r&& b<t;
     }
 }
